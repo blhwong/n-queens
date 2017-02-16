cab03d5bfc69f1b89c6706b8315f972c438488ec
blhwong <b.lh.wong@gmail.com>
2017-02-15 17:33:21 -0800

    Pass test case of countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -40,11 +40,21 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  
+  
+  var factorial = function(n) {
+    if (n === 0) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  };
 
+  var solutionCount = factorial(n); //fixme
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
+
+
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
