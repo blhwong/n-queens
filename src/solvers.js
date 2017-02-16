4358463633097ae4265a54d1cea1c8141417b883
blhwong <b.lh.wong@gmail.com>
2017-02-15 17:19:14 -0800

    Pass test case for findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,23 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  //var board = new Board({n: n});
+  var board = new Board({n: n}); //fixme
+  var solution = [];
+  for (var row = 0; row < n; row++) {
+
+    for (var col = 0; col < n; col++) {
+      
+      board.togglePiece(row, col);
+      if (board.hasAnyRooksConflicts()) {
+        board.togglePiece(row, col);
+      }
+    }
+  }
+
+  for (var i = 0; i < n; i++) {
+    solution.push(board.attributes[i]);
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
